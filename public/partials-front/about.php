ac6e4eca43f589937f4bf9222925a485c6b21494
I KADEK ROBY ADI PUTRA <73769308+Roby98-dev@users.noreply.github.com>
2022-06-11 18:38:21 +0800

    Add files via upload

---
+++ public/partials-front/about.php
@@ -0,0 +1,45 @@
+<section class="px-2 py-10">
+  <div class="max-w-6xl mx-auto">
+    <div class="p-2">
+      <div>
+        <div class="h-full w-full md:p-2 text-center">
+          <h1 class="text-gray-600 text-2xl tracking-wider md:text-4xl font-bold">Why Experience with BBM Tours</h1>
+        </div>
+      </div>
+      <div>
+        <div class="h-full w-full md:p-2 text-center">
+          <p class="text-gray-600 md:text-md tracking-wider">BBM Tours gives you an unexpected journey with a whole experience in the destination. Our priorities are delivering our quality warm hospitality, services, experiences, and comfort to bring out the best from the destination.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="md:flex justify-center hidden">
+    <div class="max-w-6xl w-full mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 text-center text-indigo-600">
+      <div class="pt-3">
+        <img src="../images/about/icon_whytravel2.png" alt="" class="w-32 mx-auto">
+        <p>A Whole Experience in One Package</p>
+      </div>
+      <div class="pt-3">
+        <img src="../images/about/icon_whytravel2.png" alt="" class="w-32 mx-auto">
+        <p>Unexpected Journey</p>
+      </div>
+      <div class="pt-3">
+        <img src="../images/about/icon_whytravel3.png" alt="" class="w-32 mx-auto">
+        <p>Comfort and Private</p>
+      </div>
+      <div class="pt-3">
+        <img src="../images/about/icon_whytravel4.png" alt="" class="w-32 mx-auto">
+        <p>Flexible Itinerary</p>
+      </div>
+      <div class="pt-3">
+        <img src="../images/about/icon_whytravel5.png" alt="" class="w-32 mx-auto">
+        <p>Hospitality & Quality</p>
+      </div>
+      <div class="pt-3">
+        <img src="../images/about/icon_whytravel6.png" alt="" class="w-32 mx-auto">
+        <p>Personalise Service</p>
+      </div>
+    </div>
+  </div>
+</section>
