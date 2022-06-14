9c2f01491b899014a0e2a6cacf3ab6ea48204195
Roby98-dev <bbmrobi3@gmail.com>
2022-06-15 00:23:46 +0800

    Youtube

--- public/partials-front/dspl-ytb.php
+++ public/partials-front/dspl-ytb.php
@@ -1,9 +1,11 @@
 <section class="py-10">
     <div class="max-w-6xl mx-auto">
-    	<video controls autoplay loop muted playsinline class="h-96 mx-auto mb-5">
-    		<source src="https://ik.imagekit.io/poinsla/bbmtours-videos/video6143442549282440522_obf6UN1-5.mp4?ik-sdk-version=javascript-1.4.3&updatedAt=1655174465256" type="video/webm" />
-  		</video>
-        <iframe src="https://www.youtube.com/embed/l2m49DuVh1w?autoplay=1&loop=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen class="mx-auto w-full h-96"></iframe>
-        <iframe src="https://www.youtube.com/embed/2om0zQEdQO0?autoplay=1&loop=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen class="mx-auto w-full h-96"></iframe>
+        <video controls autoplay loop muted playsinline class="h-96 mx-auto mb-5">
+            <source src="https://ik.imagekit.io/poinsla/bbmtours-videos/video6143442549282440522_obf6UN1-5.mp4?ik-sdk-version=javascript-1.4.3&updatedAt=1655174465256" type="video/webm" />
+        </video>
+
+        <iframe src="https://www.youtube.com/embed/l2m49DuVh1w?autoplay=1&loop=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen class="mx-auto w-full h-96 mb-5"></iframe>
+
+        <iframe src="https://www.youtube.com/embed/2om0zQEdQO0?autoplay=1&loop=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen class="mx-auto w-full h-96 mb-5"></iframe>
     </div>
 </section>
