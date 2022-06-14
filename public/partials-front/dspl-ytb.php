c1461599783c73888c78376a28f4add82dd567fe
I KADEK ROBY ADI PUTRA <73769308+Roby98-dev@users.noreply.github.com>
2022-06-14 17:30:14 +0800

    Update dspl-ytb.php

--- public/partials-front/dspl-ytb.php
+++ public/partials-front/dspl-ytb.php
@@ -1,9 +1,8 @@
 <section class="py-10">
     <div class="max-w-6xl mx-auto">
-        <video controls autoplay loop muted playsinline class="h-96 mx-auto mb-5">
-            <source src="https://ik.imagekit.io/poinsla/bbmtours-videos/video6143442549282440522_obf6UN1-5.mp4?ik-sdk-version=javascript-1.4.3&updatedAt=1655174465256" type="video/webm" />
-        </video>
 
+        <iframe src="https://www.youtube.com/embed/l2m49DuVh1w?autoplay=1&loop=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen class="mx-auto w-full h-96 mb-5"></iframe>
+        
         <iframe src="https://www.youtube.com/embed/Lj5XJJYaveg?autoplay=1&loop=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen class="mx-auto w-full h-96 mb-5"></iframe>
 
         <iframe src="https://www.youtube.com/embed/xaK8Ajje-Mk?autoplay=1&loop=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen class="mx-auto w-full h-96 mb-5"></iframe>
