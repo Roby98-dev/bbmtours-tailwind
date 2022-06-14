d2ddc86bd651ad0860e73271a2b51b8ca28b20b3
I KADEK ROBY ADI PUTRA <73769308+Roby98-dev@users.noreply.github.com>
2022-06-14 17:19:41 +0800

    Update dspl-ytb.php

--- public/partials-front/dspl-ytb.php
+++ public/partials-front/dspl-ytb.php
@@ -4,8 +4,8 @@
             <source src="https://ik.imagekit.io/poinsla/bbmtours-videos/video6143442549282440522_obf6UN1-5.mp4?ik-sdk-version=javascript-1.4.3&updatedAt=1655174465256" type="video/webm" />
         </video>
 
-        <iframe src="https://www.youtube.com/embed/l2m49DuVh1w?autoplay=1&loop=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen class="mx-auto w-full h-96 mb-5"></iframe>
+        <iframe src="https://www.youtube.com/embed/Lj5XJJYaveg?autoplay=1&loop=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen class="mx-auto w-full h-96 mb-5"></iframe>
 
-        <iframe src="https://www.youtube.com/embed/2om0zQEdQO0?autoplay=1&loop=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen class="mx-auto w-full h-96 mb-5"></iframe>
+        <iframe src="https://www.youtube.com/embed/xaK8Ajje-Mk?autoplay=1&loop=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen class="mx-auto w-full h-96 mb-5"></iframe>
     </div>
 </section>
